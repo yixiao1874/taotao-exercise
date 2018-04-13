fcfc54dd1e028f5d266740f86d1509b69bde7da4
yixiao1874 <1499957726@qq.com>
2018-04-13 17:50:55 +0800

    dubbo服务的发布以及使用

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -0,0 +1,19 @@
+package com.taotao.controller;
+
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("item/{itemId}")
+    public TbItem getItemById(@PathVariable long itemId){
+        return itemService.getItemById(itemId);
+    }
+}
