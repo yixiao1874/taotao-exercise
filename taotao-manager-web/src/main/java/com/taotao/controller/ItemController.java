5983cba9f2465d11153e0eb76c90b63f4964cc7f
yixiao1874 <1499957726@qq.com>
2018-04-16 15:03:06 +0800

    修改配置问题

--- taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ItemController {
@@ -13,6 +14,7 @@ public class ItemController {
     private ItemService itemService;
 
     @RequestMapping("item/{itemId}")
+    @ResponseBody
     public TbItem getItemById(@PathVariable long itemId){
         return itemService.getItemById(itemId);
     }
--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -4,12 +4,15 @@ import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ItemServiceImpl implements ItemService {
     @Autowired
     private TbItemMapper tbItemMapper;
     @Override
     public TbItem getItemById(long itemId) {
-        return null;
+        return tbItemMapper.selectByPrimaryKey(itemId);
     }
 }
