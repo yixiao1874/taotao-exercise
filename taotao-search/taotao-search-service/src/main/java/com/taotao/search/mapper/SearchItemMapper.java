3d9ca5dad8b49e62a56a08cdc26e2383d5b17fe2
yixiao1874 <1499957726@qq.com>
2018-04-24 15:30:30 +0800

    solr

---
+++ taotao-search/taotao-search-service/src/main/java/com/taotao/search/mapper/SearchItemMapper.java
@@ -0,0 +1,11 @@
+package com.taotao.search.mapper;
+
+import com.taotao.common.pojo.SearchItem;
+
+import java.util.List;
+
+public interface SearchItemMapper {
+
+	List<SearchItem> getItemList();
+	SearchItem getItemById(long itemId);
+}
